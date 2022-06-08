352e8e941248ce615b4c8cd39ac87e9407e21014
Mario Alberto Gauna <gauna25@gmail.com>
2022-06-08 06:39:29 -0300

    v1.0 Funcionando con conflictos

---
+++ configChatDB.js
@@ -0,0 +1,9 @@
+const chatOP ={
+    client:'sqlite3',
+    connection:{
+        filename:'./database/ecommerce.sqlite'
+    },
+    useNullAsDefault:true
+}
+
+module.exports = {chatOP}
---
+++ configProdDB.js
@@ -0,0 +1,13 @@
+const prodOP = {
+        client:'mysql',
+        connection:{
+            host: "127.0.0.1",
+            user: "root",
+            database: "warehouse",
+        },
+    pool:{min:0,max:10},
+}
+
+
+
+module.exports = {prodOP}
